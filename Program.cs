596d933d6453a3c9c9e79221cf73a7e300c7eb02
Norbex <Norbex@DESKTOP-HE70ISR>
2024-05-28 15:43:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Szamologep_lite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.0 verzió esetében angol nyelv eltávolításra került! Később másik app lesz az angol nyelv");
+            Console.WriteLine("Kérek egy számot!");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            double b = Convert.ToInt32(Console.ReadLine());
+            double osszeadas = Math.Round(a + b, 4);
+            double kivonas = Math.Round(a - b, 4);
+            double szorzas = Math.Round(a * b, 4);
+            double osztas = Math.Round(a / b, 4);
+
+            //Változókkal való müveletek eredményének a kiiratása!
+            Console.WriteLine($"Az összeadásának eredménye: {osszeadas}");
+            Console.WriteLine($"A kivonásának eredménye: {kivonas}");
+            Console.WriteLine($"A szorzásának eredménye: {szorzas}");
+            Console.WriteLine($"Az osztásának eredménye: {osztas}");
+            Console.ReadKey();
+        }
+    }
+}
